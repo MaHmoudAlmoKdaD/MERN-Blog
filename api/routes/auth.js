6f6fbfa9b3f8de516f03f70be6ec7f49848c1396
Mahmoud <mahmoudalmokdadtechnology@gmail.com>
2022-09-27 12:33:32 +0300

    upload

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+async function hashedPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+  return hashedPass;
+}
 
 //REGISTER
 
 router.post("/register", async (req, res) => {
   try {
+    const hashedPass = await hashedPassword(req.body.password);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPass,
     });
     const user = await newUser.save();
     res.status(200).json(user);
@@ -18,5 +26,19 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials");
 
-module.exports = router;
+    const validated = await bcrypt.compare(req.body.password, user.username);
+    !validated && res.status(400).json("Wrong credentials");
+
+    const { password, ...others } = user;
+    res.status(200).json(others);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router; 
