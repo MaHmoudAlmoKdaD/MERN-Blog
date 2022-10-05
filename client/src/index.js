33824cf6f4051c7e582a11c37e0fcefc3d6d7026
Mahmoud <mahmoudalmokdadtechnology@gmail.com>
2022-10-05 12:07:58 +0300

    create context

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
+import { ContextProvider } from "./context/Context";
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = "http://localhost:5000/api";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,23 +1,60 @@
 import "./login.css";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { useContext } from "react";
+import { Context } from "./../../context/Context";
+import axios from "axios";
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    axios
+      .post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      })
+      .then((response) => {
+        dispatch({
+          type: "LOGIN_SUCCESS",
+          payload: response.data,
+        });
+      })
+      .catch((error) => {
+        dispatch({
+          type: "LOGIN_FAILURE",
+        });
+      });
+      console.log(user)
+      console.log(isFetching)
+  };
+  console.log(user)
   return (
-    <div className="login">
+    <div className="login">{console.log("rendering")}
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
         <input
           type="text"
           className="loginInput"
-          placeholder="Enter your email"
+          placeholder="Enter your username"
+          required
+          ref={userRef}
         />
         <label>Password</label>
         <input
-          type="text"
+          type="password"
           className="loginInput"
           placeholder="Enter your password"
+          required
+          ref={passwordRef}
         />
-        <button className="loginButton">Login</button>
+        <button className="loginButton" type="submit">
+          Login
+        </button>
       </form>
       <button className="loginRegisterButton">
         <Link to="/register" className="link">
